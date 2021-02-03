e5ecbe0ddbc6bd85d9fdd37b375fbedf470bd734
Masanori Igarashi <masanori.igarashi1@gmail.com>
2021-02-04 07:22:07 +0900

    created a window

--- 03/Project1/Project1/main.cpp
+++ 03/Project1/Project1/main.cpp
@@ -1,10 +1,13 @@
 #include <Windows.h>
 #include <cstdio>
+#include <tchar.h>
 #ifdef _DEBUG
 #include <iostream>
 #endif // _DEBUG
 
 using namespace std;
+
+static LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
 
 void DebugOutputFormatString(const char* format, ...)
 {
@@ -25,6 +28,65 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 #endif // _DEBUG
 	DebugOutputFormatString("Show window test.");
 	getchar();
+
+	WNDCLASSEX w = { };
+	{
+		w.cbSize = sizeof(WNDCLASSEX);
+		w.lpfnWndProc = (WNDPROC)WindowProcedure;
+		w.lpszClassName = _T("DX12Sample");
+		w.hInstance = GetModuleHandle(nullptr);
+	}
+
+	RegisterClassEx(&w);
+
+	const long window_width = 1920;
+	const long window_height = 1080;
+	RECT wrc = { 0, 0, window_width, window_height };
+
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+	HWND hwnd = CreateWindow(w.lpszClassName,
+		_T("DX12_test"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		wrc.right - wrc.left,
+		wrc.bottom - wrc.top,
+		nullptr,
+		nullptr,
+		w.hInstance,
+		nullptr);
+
+	ShowWindow(hwnd, SW_SHOW);
+
+	MSG msg = {};
+
+	while (true)
+	{
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		if (msg.message == WM_QUIT)
+		{
+			break;
+		}
+	}
+
+	UnregisterClass(w.lpszClassName, w.hInstance);
+
 	return 0;
 }
 
+LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	if (msg == WM_DESTROY)
+	{
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
