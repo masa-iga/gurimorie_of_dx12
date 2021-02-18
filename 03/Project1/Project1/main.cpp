47177fa6127f98f649fc2f50699a881ddcef5f7d
Masanori Igarashi <masanori.igarashi1@gmail.com>
2021-02-19 07:24:40 +0900

    show FPS

--- 03/Project1/Project1/main.cpp
+++ 03/Project1/Project1/main.cpp
@@ -1,6 +1,7 @@
 #include <Windows.h>
 #include <cstdio>
 #include <cassert>
+#include <vector>
 #include <tchar.h>
 #include "init.h"
 #include "debug.h"
@@ -10,6 +11,7 @@
 using namespace std;
 
 static LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+static void trackFrameTime(UINT frame);
 
 #ifdef _DEBUG
 int main()
@@ -77,6 +79,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		{
 			break;
 		}
+
+		trackFrameTime(i);
 	}
 
 	UnregisterClass(w.lpszClassName, w.hInstance);
@@ -93,5 +97,61 @@ LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 	}
 
 	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
+class FrameCounter
+{
+public:
+	static constexpr UINT kNumOfTrack = 600;
+	FrameCounter();
+
+	void track();
+	UINT64 getInUsec() const;
+
+private:
+	LARGE_INTEGER m_freq = {};
+	std::vector<LARGE_INTEGER> m_times;
+	UINT m_idx = 0;
+};
+
+FrameCounter::FrameCounter()
+	: m_times(kNumOfTrack)
+{
+	ThrowIfFalse(QueryPerformanceFrequency(&m_freq));
+};
+
+void FrameCounter::track()
+{
+	m_idx = (m_idx + 1) % kNumOfTrack;
+	auto& time = m_times.at(m_idx);
+	ThrowIfFalse(QueryPerformanceCounter(&time));
+}
+
+UINT64 FrameCounter::getInUsec() const
+{
+	constexpr UINT kPeriod = 60;
+	static_assert(kPeriod <= kNumOfTrack);
+
+	auto prev_idx = (m_idx + kNumOfTrack - kPeriod) % kNumOfTrack;
+	const auto previous = m_times.at(prev_idx).QuadPart;
+	const auto current = m_times.at(m_idx).QuadPart;
+
+	auto elapsed = (current - previous) / kPeriod;
+	elapsed *= 1'000'000; // to usec
+	elapsed /= m_freq.QuadPart;
+
+	return elapsed;
+}
+
+void trackFrameTime(UINT frame)
+{
+	static FrameCounter frameCounter;
+
+	frameCounter.track();
+
+	if (frame != 0 && frame % 60 == 0)
+	{
+		DebugOutputFormatString("FPS %.1f\n", 1'000'000.0f / frameCounter.getInUsec());
+	}
 }
 
