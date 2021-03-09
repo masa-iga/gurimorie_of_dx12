b8ad7f6002270635458b76558fc9d2f9a53fa274
Masanori Igarashi <masanori.igarashi1@gmail.com>
2021-03-10 07:25:28 +0900

    Revert "just replaced std::vector with std::array"
    This reverts commit 602d8c8b410ecd9bf5d2e512dc98d25547eeed75.

--- code/chap03/main.cpp
+++ code/chap03/main.cpp
@@ -1,7 +1,7 @@
 #include <Windows.h>
-#include <array>
 #include <cstdio>
 #include <cassert>
+#include <vector>
 #include <tchar.h>
 #include "init.h"
 #include "debug.h"
@@ -110,11 +110,12 @@ public:
 
 private:
 	LARGE_INTEGER m_freq = {};
-	std::array<LARGE_INTEGER, kNumOfTrack> m_times;
+	std::vector<LARGE_INTEGER> m_times;
 	UINT m_idx = 0;
 };
 
 FrameCounter::FrameCounter()
+	: m_times(kNumOfTrack)
 {
 	ThrowIfFalse(QueryPerformanceFrequency(&m_freq));
 };
