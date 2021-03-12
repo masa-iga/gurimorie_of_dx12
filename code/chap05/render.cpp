34b60d933e1d3fab196b0302b18ade5a68a0e63f
Masanori Igarashi <masanori.igarashi1@gmail.com>
2021-03-13 08:16:58 +0900

    created a descriptor heap and shader resource view for redaing a texture

--- code/chap05/render.cpp
+++ code/chap05/render.cpp
@@ -28,8 +28,17 @@ HRESULT Render::init()
 	ThrowIfFailed(createVertexBuffer());
 	ThrowIfFailed(loadShaders());
 	ThrowIfFailed(loadImage());
-	ThrowIfFailed(createTextureBuffer());
-	ThrowIfFailed(createTextureBuffer2());
+
+	constexpr bool bUseCopyTextureRegion = true;
+	if (bUseCopyTextureRegion)
+	{
+		ThrowIfFailed(createTextureBuffer2());
+	}
+	else
+	{
+		ThrowIfFailed(createTextureBuffer());
+	}
+
 	ThrowIfFailed(createPipelineState());
 
 	return S_OK;
@@ -625,6 +634,38 @@ HRESULT Render::createTextureBuffer2()
 	// reset command allocator & list
 	ThrowIfFailed(getInstanceOfCommandAllocator()->Reset());
 	ThrowIfFailed(getInstanceOfCommandList()->Reset(getInstanceOfCommandAllocator(), nullptr));
+
+
+
+	// create a descriptor heap for shader resource
+	D3D12_DESCRIPTOR_HEAP_DESC descHeapDesc = { };
+	{
+		descHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+		descHeapDesc.NumDescriptors = 1;
+		descHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+		descHeapDesc.NodeMask = 0;
+	}
+	auto ret = getInstanceOfDevice()->CreateDescriptorHeap(
+		&descHeapDesc,
+		IID_PPV_ARGS(&m_texDescHeap));
+	ThrowIfFailed(ret);
+
+	// create a shader resource view on the heap
+	D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc = { };
+	{
+		srvDesc.Format = m_metadata.format;
+		srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
+		srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+		srvDesc.Texture2D.MostDetailedMip = 0;
+		srvDesc.Texture2D.MipLevels = 1;
+		srvDesc.Texture2D.PlaneSlice = 0;
+		srvDesc.Texture2D.ResourceMinLODClamp = 0.0f;
+	}
+	getInstanceOfDevice()->CreateShaderResourceView(
+		texBuff,
+		&srvDesc,
+		m_texDescHeap->GetCPUDescriptorHandleForHeapStart()
+	);
 
 	return S_OK;
 }
