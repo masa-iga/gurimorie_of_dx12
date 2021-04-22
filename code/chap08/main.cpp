bdf0cc4073046260a443d561a47cd3d0de145b58
Masanori Igarashi <masanori.igarashi1@gmail.com>
2021-04-23 07:37:05 +0900

    started to receive key input

--- code/chap08/main.cpp
+++ code/chap08/main.cpp
@@ -11,6 +11,7 @@
 using namespace std;
 
 static LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+static void processKeyInput(const MSG& msg);
 static void trackFrameTime(UINT frame);
 
 #ifdef _DEBUG
@@ -78,6 +79,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 			break;
 		}
 
+		processKeyInput(msg);
+
 		trackFrameTime(i);
 	}
 
@@ -95,6 +98,23 @@ LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 	}
 
 	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
+static void processKeyInput(const MSG& msg)
+{
+	switch (msg.message) {
+	case WM_KEYDOWN:
+		switch (msg.wParam) {
+		case VK_SPACE:
+			DebugOutputFormatString("space was pushed.\n");
+			break;
+		default:
+			break;
+		}
+		break;
+	default:
+		break;
+	}
 }
 
 class FrameCounter
