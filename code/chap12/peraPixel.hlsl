44ca0454c413d19c04724853705903762f750f2a
Masanori Igarashi <masanori.igarashi1@gmail.com>
2021-07-18 08:18:47 +0900

    simple filter in PS

--- code/chap12/peraPixel.hlsl
+++ code/chap12/peraPixel.hlsl
@@ -1,9 +1,10 @@
 #include "peraHeader.hlsli"
 
-#define NORMAL (1)
+#define NORMAL (0)
 #define GREYSCALE (0)
 #define INVERSION (0)
 #define DOWNGRADE (0)
+#define FILTER (1)
 
 float4 main(Output input) : SV_TARGET
 {
@@ -25,6 +26,27 @@ float4 main(Output input) : SV_TARGET
 #if DOWNGRADE
 	return float4(col.rgb - fmod(col.rgb, 0.25f), col.a);
 #endif // DOWNGRADE
+
+#if FILTER
+	float w, h, levels;
+	tex.GetDimensions(0 /* mip level*/, w, h, levels);
+
+	const float dx = 1.0f / w;
+	const float dy = 1.0f / h;
+	float4 ret = float4(0.0f, 0.0f, 0.0f, 0.0f);
+
+	ret += tex.Sample(smp, input.uv + float2(-2.0f * dx, -2.0f * dy));
+	ret += tex.Sample(smp, input.uv + float2(0.0f, -2.0f * dy));
+	ret += tex.Sample(smp, input.uv + float2(2.0f * dx, -2.0f * dy));
+	ret += tex.Sample(smp, input.uv + float2(-2.0f * dx, 0.0f));
+	ret += tex.Sample(smp, input.uv);
+	ret += tex.Sample(smp, input.uv + float2(2.0f * dx, 0.0f));
+	ret += tex.Sample(smp, input.uv + float2(-2.0f * dx, 2.0f * dy));
+	ret += tex.Sample(smp, input.uv + float2(0.0f, 2.0f * dy));
+	ret += tex.Sample(smp, input.uv + float2(2.0f * dx, 2.0f * dy));
+
+	return ret / 9.0f;
+#endif // FILTER
 
 	return float4(1.0, 1.0, 1.0, 1.0);
 }
